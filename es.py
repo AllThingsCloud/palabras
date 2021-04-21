8052243bd1eccbe3ab83c3bbe0dda9f8c88b640b
soyroberto <roberto@rober.to>
2021-04-21 14:01:42 +1000

    feliz

--- es.py
+++ es.py
@@ -25,10 +25,6 @@ else:
 		#originalprint("\n" + r.text)
 		print(r.text)
 #json lee
-		#alist = json.loads(r.json())
-		#print(type(r))
-		#print(r[0])
-		#print(alist[0]['word'])
 		#https://stackoverflow.com/questions/4706499/how-do-you-append-to-a-file
 		f = open ('/Users/roberto/t/es.json','a') 
 		f.write(r.text)
