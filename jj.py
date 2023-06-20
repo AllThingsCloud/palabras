5fc1940409f61696ff83013e1511a5d962a34d7b
soyroberto <roberto@rober.to>
2023-06-20 18:45:29 +1000

    jj

--- jj.py
+++ jj.py
@@ -3,6 +3,10 @@ import json
 with open('amor.json', 'r') as f:
 	data = json.load(f)
 
-print (data["results"])
+#print (data["results"])
+#print (type(data))
+for pal in data["results"]:
+#	print (pal["lexicalEntries"])
+	for ent in (pal["lexicalEntries"]):
+		print (ent["entries"])
 
-#results[].lexicalEntries[]
