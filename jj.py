625a92193aec7496680216267cc529ccd34d5284
soyroberto <roberto@rober.to>
2023-06-22 18:23:27 +1000

    def jf (*args):

--- jj.py
+++ jj.py
@@ -1,15 +1,22 @@
 import json
+import pprint
+import re
 
+#jq '.results[].lexicalEntries[0].entries[0].senses[0].definitions[0]'
 with open('amor.json', 'r') as f:
 	data = json.load(f)
 
-for pal in data["results"]:
-	for ent in (pal["lexicalEntries"]):
-		for senses in (ent["entries"]):
-			definition= senses["senses"]
-			print(definition)
-			#print (senses["senses"])
-            
-			#for definitions in (senses["definitions"]):
-			#	print(json.dump(definitions,indent=1))
+def jf (*args):
+	for pal in data["results"]:
+		for ent in (pal["lexicalEntries"]):
+			for senses in (ent["entries"]):
+				for defi in (senses["senses"]):
+					definition = senses["senses"]
+					#print(defi) #imprime
+					#return senses
+					return definition
+				
+r = jf(data)
+#print (r)
 
+pprint.pprint(r)
